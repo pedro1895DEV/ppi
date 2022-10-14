33ef05e69fa500da00d5ee052222105d41bd344d
pedro1895DEV <95310737+pedro1895DEV@users.noreply.github.com>
2022-10-14 11:13:51 -0300

    update

--- atividades-30-09/atividade8.js
+++ atividades-30-09/atividade8.js
@@ -1,21 +1,23 @@
-let pessoa ={
+let pessoa = {
     primeiroNome : '',
     ultimoNome : '',
 
-    get nome(){
-        return this.primeiroNome;
-    },
-    set nome(primeiroNome){
-        primeiroNome = 'Lucas';
-    },
-    get total (){
-        return this.ultimoNome;
-    },
-    set total(ultimoNome){
-        return ultimoNome = 'Barbosa';
-    }
-
+  get nome (){
+    return this.primeiroNome
+  },
+  set nome(novoPrimeiro){
+    this.primeiroNome = novoPrimeiro
+  },
+  get nome (){
+    return this.ultimoNome
+  },
+  set nome(novoUltimo){
+    this.ultimoNome = novoUltimo
+  }
 }
-pessoa.primeiroNome = 'Lucas';
+pessoa.primeiroNome = 'Lucas ';
 pessoa.ultimoNome = 'Barbosa';
+console.log(pessoa.nome);
+
+pessoa.nome = 'Mariana'
 console.log(pessoa.nome);
