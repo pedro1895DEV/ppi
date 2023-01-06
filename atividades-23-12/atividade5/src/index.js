9dfbfd230b19ae38737a339292ba2c7472f3d1f9
pedro1895DEV <95310737+pedro1895DEV@users.noreply.github.com>
2023-01-06 19:48:42 -0300

    update

--- atividades-23-12/atividade5/src/index.js
+++ atividades-23-12/atividade5/src/index.js
@@ -1,2 +1,11 @@
-let space = require('to-space-case');
+var space = require('to-space-case');
 
+module.exports = {
+    minhaFuncao: function () {
+        $('#resultado').text(
+            space(
+                $('#dados').val()
+            )
+        );
+    },
+}
