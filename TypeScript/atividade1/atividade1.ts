4b21fc0741249ad667d8c1d76fd9a418f53e4fb5
pedro1895DEV <95310737+pedro1895DEV@users.noreply.github.com>
2023-02-14 22:36:51 -0300

    ts

--- TypeScript/atividade1/atividade1.ts
+++ TypeScript/atividade1/atividade1.ts
@@ -5,4 +5,4 @@ console.log(`${sistema}, versão ${versao}, ${lts? 'LTS' : ''}`);
 
 let pi = 3.14159;
 let tau = pi * 2;
-console.log(`${tau} é igual a ${pi} vezes dois.`);
+console.log(`${tau} é igual a ${pi} vezes doiiiiiis.`);
---
+++ TypeScript/atividade2/atividade2.ts
@@ -0,0 +1,12 @@
+let sabor: string;
+sabor = 'chocolate';
+
+function escreveSabor(sab: string) {
+    console.log(`Pizza sabor ${sab}`);
+}
+
+escreveSabor(sabor);
+
+let registrado: number;
+registrado = 1;
+console.log(`${registrado ? 'Usuário registrado' : 'Usuário não registrado'}`);
