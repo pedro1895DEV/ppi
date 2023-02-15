acb822218efa5b9ea3d388e695fdd8f735595bef
pedro1895DEV <95310737+pedro1895DEV@users.noreply.github.com>
2023-02-14 23:12:18 -0300

    ts

---
+++ TypeScript/atividade3/atividade3.ts
@@ -0,0 +1,6 @@
+const numeros : number[] = [1, 2, 3, 4, 5];
+const animais : string [] = ['tamanduá', 'tatu', 'onça', 'paca'];
+const misto : (number | string)[] = [1, 'um', 2, 'dois', 3, 'três'];
+const matriz : (any)[] = [numeros, animais, misto];
+
+console.log(matriz);
