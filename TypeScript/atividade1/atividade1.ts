8c59ee98d2c4984f64871568ec68edce913e9a8c
pedro1895DEV <95310737+pedro1895DEV@users.noreply.github.com>
2023-02-14 22:18:35 -0300

    ts

---
+++ TypeScript/atividade1/atividade1.ts
@@ -0,0 +1,8 @@
+let sistema : string = 'Ubuntu';
+let versao : number = 19.10;
+let lts : number = 1;
+console.log(`${sistema}, versão ${versao}, ${lts? 'LTS' : ''}`);
+
+let pi = 3.14159;
+let tau = pi * 2;
+console.log(`${tau} é igual a ${pi} vezes dois.`);
