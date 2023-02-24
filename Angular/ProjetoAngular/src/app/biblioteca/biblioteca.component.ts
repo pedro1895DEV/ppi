ba18ffd78b85a4062d973ef49e31f6b2ae424ffe
pedro1895DEV <95310737+pedro1895DEV@users.noreply.github.com>
2023-02-24 16:13:40 -0300

    Projeto finalizado

---
+++ Angular/ProjetoAngular/src/app/biblioteca/biblioteca.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-biblioteca',
+  templateUrl: './biblioteca.component.html',
+  styleUrls: ['./biblioteca.component.css']
+})
+export class BibliotecaComponent {
+  @Input() usuario = '';
+  @Input() tipo = '';
+  cor = 'roxo';
+
+  muda() {
+    this.tipo = 'Premium';
+    
+    if(this.tipo === "Premium"){
+      this.cor = 'verde'
+    }
+    if(this.tipo === "Padrão"){
+      this.cor = "roxo"
+    }
+  }
+
+  desmuda(){
+    this.tipo = 'Padrão';
+    
+    if(this.tipo === "Premium"){
+      this.cor = 'verde'
+    }
+    if(this.tipo === "Padrão"){
+      this.cor = "roxo"
+    }
+  }
+}
